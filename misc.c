cb2a806a04edb15dbf6c9f3085fc8da0e7a870e4
click <click@antifork.org>
2003-05-07 13:49:09 +0000

    indent

--- misc.c
+++ misc.c
@@ -1,25 +1,26 @@
 #include "fibc.h"
 
-void 
-usage(char *name)
+void
+usage (char *name)
 {
-	fprintf(stderr, " Usage: %s -b -r [-a] [-s] -m -g -n [-d] -f -c\n\n", name);
-	fprintf(stderr, " b: net scanner\n");
-	fprintf(stderr, " r: route IP		mandatory\n");
-	fprintf(stderr, " a: read file		optional\n");
-	fprintf(stderr, " s: spoofed source	optional\n");
-	fprintf(stderr, " m: metric's value	default: 1\n");
-	fprintf(stderr, " g: gateway IP		default: local machine\n");
-	fprintf(stderr, " n: netmask		default: 255.255.255.0\n");
-	fprintf(stderr, " d: daemonize		optional\n");
-	fprintf(stderr, " f: force		default: don't force\n");
-	fprintf(stderr, " c: check route		default: no\n");
-	fprintf(stderr, " h: this help\n\n");
-	exit(1);
+  fprintf (stderr, " Usage: %s -b -r [-a] [-s] -m -g -n [-d] -f -c\n\n",
+	   name);
+  fprintf (stderr, " b: net scanner\n");
+  fprintf (stderr, " r: route IP		mandatory\n");
+  fprintf (stderr, " a: read file		optional\n");
+  fprintf (stderr, " s: spoofed source	optional\n");
+  fprintf (stderr, " m: metric's value	default: 1\n");
+  fprintf (stderr, " g: gateway IP		default: local machine\n");
+  fprintf (stderr, " n: netmask		default: 255.255.255.0\n");
+  fprintf (stderr, " d: daemonize		optional\n");
+  fprintf (stderr, " f: force		default: don't force\n");
+  fprintf (stderr, " c: check route		default: no\n");
+  fprintf (stderr, " h: this help\n\n");
+  exit (1);
 }
 
-void 
-credits()
+void
+credits ()
 {
-	printf("\n RiPPeR v.1.1-beta\tby mydecay && click\n\n");
+  printf ("\n RiPPeR v.1.1-beta\tby mydecay && click\n\n");
 }
